44ad092990fb2a96336a8041f4bfcd05b13d3444
Erick Clinton <erickleite2009321@gmail.com>
2022-10-05 09:28:15 -0300

    Adding numbers of the task

---
+++ src/components/Conclusion.tsx
@@ -0,0 +1,10 @@
+import styles from './Conclusion.module.css'
+
+export function Conclusion(){
+    return(
+        <div className={styles.teste}>
+            <p className={styles.tasks}>tarefas concluidas 0</p>
+            <p className={styles.conclusion}>Concluído 0</p>
+        </div>
+    )
+}
