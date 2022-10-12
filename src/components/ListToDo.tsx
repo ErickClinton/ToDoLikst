f2b94d5a1d68fb28146fe50be1a274bf09d3adad
Erick Clinton <erickleite2009321@gmail.com>
2022-10-12 10:36:55 -0300

    Add styles for tasks

---
+++ src/components/ListToDo.tsx
@@ -0,0 +1,14 @@
+import styles from './ListToDo.module.css'
+import { Trash } from 'phosphor-react'
+export function ListToDo(){
+    return(
+        <div>
+            <div className={styles.listStyle}>
+                <button className={styles.CheckList}/>
+                <p>preciso fazer isso funcionar</p> 
+                        
+            </div>
+            <button className={styles.buttonTrash}><Trash /></button> 
+        </div>
+    )
+}
